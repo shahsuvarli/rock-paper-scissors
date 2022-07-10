c6ea54a9781f1f8dec8800a960b7fc9538e6ab38
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-07-10 19:46:08 +0400

    init

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Input from "./Input";
+
+export default function Game() {
+  return (
+    <div>
+      <h1>Rock-Paper-Scissors</h1>
+      <Input />
+    </div>
+  );
+}
